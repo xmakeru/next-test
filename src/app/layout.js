1ef5074cf26215361d83ba002ec0e8bdca694c65
xmakeru <alyonasubbotina2000@gmail.com>
2024-06-29 04:03:55 -0700

    1

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Poppins } from 'next/font/google'
 import "./globals.css"
-import Header from "./components/Header";
+import Header from './components/header'
 
 const poppins = Poppins({
   weight: ['400', '700'],
